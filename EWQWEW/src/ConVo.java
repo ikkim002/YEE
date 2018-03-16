560ac603bd32ac6a8b757178bc15decaf4abb9b4
USER <USER@INET-07>
2018-03-16 11:47:19 +0900

    yrr

--- EWQWEW/src/ConVo.java
+++ EWQWEW/src/ConVo.java
@@ -13,17 +13,16 @@ public class ConVo {
 		for (int i = 0; i < s.length(); i++) {
 			s2 = s.charAt(i);
 			if (s2 >= 'A' && s2 <= 'Z')
-			else if (s2 == 'a' || s2 == 'e' || s2 == 'i' || s2 == 'o' || s2 == 'u');
-				else if (s2 == '1' || s2 == '2' || s2 == '3' || s2 == '4' || s2 == '5');
+				 if (s2 == 'a' || s2 == 'e' || s2 == 'i' || s2 == 'o' || s2 == 'u');
+					 if (s2 == '1' || s2 == '2' || s2 == '3' || s2 == '4' || s2 == '5');
 					count1++;
-					else
-						count2++;
-
+		else
+					count2++;
 			}
 
 		System.out.println("대문자의 개수 : " + count2);
 		System.out.println("소문자의 개수 : " + count1);
 		System.out.println("숫자의 개수 : " + count3);
 	}
-
+}
 }
